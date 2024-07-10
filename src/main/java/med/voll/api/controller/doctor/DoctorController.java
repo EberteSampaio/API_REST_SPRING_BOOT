cc6014f8b8e4b431dba03231b0975f77c322056b
EberteSampaio <ebertechavessampaiojr@gmail.com>
2024-07-10 01:40:29 -0300

    feat: Creating a Method to Save a New Doctor on database

--- src/main/java/med/voll/api/controller/doctor/DoctorController.java
+++ src/main/java/med/voll/api/controller/doctor/DoctorController.java
@@ -1,14 +1,22 @@
 package med.voll.api.controller.doctor;
 
+import med.voll.api.entity.Address;
+import med.voll.api.entity.Doctor;
+import med.voll.api.interfaces.repository.DoctorRepository;
 import med.voll.api.record.doctor.MedicalRegistrationData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("medicos")
 public class DoctorController {
+    @Autowired
+    private  DoctorRepository doctorRepository;
 
     @PostMapping
+    @Transactional
     public void create(@RequestBody MedicalRegistrationData doctor ){
-        System.out.println(doctor);
+         this.doctorRepository.save(new Doctor(doctor));
     }
 }
