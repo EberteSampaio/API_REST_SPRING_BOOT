f2c267468fbb6be0e6ce46285a1eefc32ada579e
EberteSampaio <ebertechavessampaiojr@gmail.com>
2024-07-10 01:40:18 -0300

    feat: Creating a Method to Save a New Doctor on database

--- src/main/java/med/voll/api/interfaces/repository/DoctorRepository.java
+++ src/main/java/med/voll/api/interfaces/repository/DoctorRepository.java
@@ -2,6 +2,6 @@ package med.voll.api.interfaces.repository;
 import med.voll.api.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface doctorRepository extends JpaRepository<Doctor,Long> {
+public interface DoctorRepository extends JpaRepository<Doctor,Long> {
 
 }
