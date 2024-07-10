0463da31df05c656c7af695718eeddc4d39c3385
EberteSampaio <ebertechavessampaiojr@gmail.com>
2024-07-10 01:35:36 -0300

    feat: import lombok

--- src/main/java/med/voll/api/entity/Address.java
+++ src/main/java/med/voll/api/entity/Address.java
@@ -1,8 +1,15 @@
 package med.voll.api.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.record.others.AddressData;
 
 @Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     private String streetAddress;
     private String neighborhood;
@@ -11,4 +18,17 @@ public class Address {
     private String uf;
     private String houseNumber;
     private String complement;
+
+    public Address(AddressData addressData) {
+        this.streetAddress  = addressData.streetAddress();
+        this.neighborhood   = addressData.neighborhood();
+        this.zipCode        = addressData.zipCode();
+        this.city           = addressData.city();
+        this.uf             = addressData.uf();
+        this.houseNumber    = addressData.houseNumber();
+        this.complement     = addressData.complement();
+
+    }
+
+
 }
