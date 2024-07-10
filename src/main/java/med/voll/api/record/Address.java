27e6db42fe8335c4b619276561509e89a7a426be
EberteSampaio <ebertechavessampaiojr@gmail.com>
2024-07-09 21:03:19 -0300

    fix: correct parameters in Address.java record

--- src/main/java/med/voll/api/record/Address.java
+++ src/main/java/med/voll/api/record/Address.java
@@ -3,6 +3,8 @@ package med.voll.api.record;
 public record Address(String streetAddress,
                       String neighborhood,
                       Integer zipCode,
+                      String city,
+                      String uf,
                       String houseNumber,
                       String complement) {
 }
