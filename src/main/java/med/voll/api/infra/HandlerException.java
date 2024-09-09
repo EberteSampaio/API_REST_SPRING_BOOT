2ba3eba975bb2455b02c9ec4e96163a322ae0f7e
EberteSampaio <ebertechavessampaiojr@gmail.com>
2024-09-08 22:01:50 -0300

    feat: Returns validations and their respective errors

--- src/main/java/med/voll/api/infra/HandlerException.java
+++ src/main/java/med/voll/api/infra/HandlerException.java
@@ -2,8 +2,12 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class HandlerException {
@@ -13,5 +17,21 @@ public class HandlerException {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorValidation>> error400Exception(MethodArgumentNotValidException exception){
+        List<FieldError> errors = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest()
+                .body(
+                errors.stream()
+                        .map(ErrorValidation::new)
+                        .toList());
+    }
+
+    public record ErrorValidation(String input, String message){
+        public ErrorValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 
 }
