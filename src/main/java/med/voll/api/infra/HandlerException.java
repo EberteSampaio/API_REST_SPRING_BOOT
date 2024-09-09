8e267e072dfbfb142f83c52fcbd601bc4941c29e
EberteSampaio <ebertechavessampaiojr@gmail.com>
2024-09-08 21:38:12 -0300

    feat: Creating class HandlerException to handle exceptions in all Controllers

---
+++ src/main/java/med/voll/api/infra/HandlerException.java
@@ -0,0 +1,17 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class HandlerException {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<HandlerException> error404Exception(){
+        return ResponseEntity.notFound().build();
+    }
+
+
+}
